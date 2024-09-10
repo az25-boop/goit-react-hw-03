cf43ff615676453418967fff70281a7770855131
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-09-10 09:53:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
-import Contact from "./components/Contact/Contact";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import s from "./App.css";
-import contact from "./contact.json";
+import initialContacts from "./contact.json";
 
 export default function App() {
   const [contacts, setContacts] = useState(() => {
@@ -39,8 +38,8 @@ export default function App() {
       <div className={s.app}>
         <h1>Phonebook</h1>
         <ContactForm onSubmit={handleSubmit} />
-        <SearchBox  filter={filter} onFilter={setFilter}/>
-        <ContactList contacts={visibleContacts} onDelete={deleteContact}/>
+        <SearchBox filter={filter} onFilter={setFilter} />
+        <ContactList contacts={visibleContacts} onDelete={deleteContact} />
       </div>
     </>
   );
